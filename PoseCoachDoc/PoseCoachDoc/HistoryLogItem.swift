8010c4d5e1ddc92d8b3c9dfb3d10a62d0b4a3d8a
Dei Ren Wu <deirenwu1101@gmail.com>
2021-12-20 17:44:26 +0800

    Add `HistoryLogItem`

---
+++ PoseCoachDoc/PoseCoachDoc/HistoryLogItem.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryLogItem.swift
+//  PoseCoachDoc
+//
+//  Created by 吳得人 on 2021/12/20.
+//
+
+import Foundation
+
+struct HistoryLogItem {
+    let title: String
+    let description: String?
+    let timestamp: Date
+}
