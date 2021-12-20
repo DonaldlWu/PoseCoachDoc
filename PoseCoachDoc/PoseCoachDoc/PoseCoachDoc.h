ebb5e549a684020674773b6a7b8abdf9e56d6128
Dei Ren Wu <deirenwu1101@gmail.com>
2021-12-20 17:35:53 +0800

    Initial commit for PoseCoachDoc framework

---
+++ PoseCoachDoc/PoseCoachDoc/PoseCoachDoc.h
@@ -0,0 +1,18 @@
+//
+//  PoseCoachDoc.h
+//  PoseCoachDoc
+//
+//  Created by 吳得人 on 2021/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PoseCoachDoc.
+FOUNDATION_EXPORT double PoseCoachDocVersionNumber;
+
+//! Project version string for PoseCoachDoc.
+FOUNDATION_EXPORT const unsigned char PoseCoachDocVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PoseCoachDoc/PublicHeader.h>
+
+
